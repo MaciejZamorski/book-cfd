bad9dd91a825252696d7e3136c86574ff39c29e8
Carlos Pereira <carlosp8@gmail.com>
2020-01-07 12:01:16 -0500

    Added Chaos notebook and simplified FD/FV schemes notebooks.

--- notebooks/nbtools.py
+++ notebooks/nbtools.py
@@ -13,18 +13,6 @@ import matplotlib.ticker as ticker
 
 plt.style.use('../code/style.mplstyle')
 
-def init_plot(x, y):    
-    fig, ax = plt.subplots(figsize=(5,4))
-    ax.set_xlabel('$x$')
-    ax.set_ylabel('$u(x,t)$')
-    return fig, ax
-
-def update_plot(y, fig, line):
-    line.set_ydata(y)
-    display.clear_output(wait=True)
-    display.display(fig)
-    time.sleep(0.001)
-
 def get_colors(n, start=0.2, end=1, rev=True):
     if rev:
         return plt.cm.gist_heat_r(np.linspace(start, end, n))
